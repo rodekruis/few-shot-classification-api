741a2f5b4b9e2458bc92a885cadee07d9a017018
jmargutti <jacopo.margutti@gmail.com>
2022-09-02 15:51:21 +0200

    live link to model

--- classification_demo.py
+++ classification_demo.py
@@ -12,9 +12,10 @@ def build_prompt(class_names, class_examples, input_text):
     model_prompt += f"Class:"
     return model_prompt
 
+model_req = requests.get('http://language-model.westeurope.cloudapp.azure.com/model').json()
 
 st.header("text-classification-app")
-st.markdown("Use language models to classify text.")
+st.markdown(f"Use language models to classify text. Currently powered by [{model_req['model_name']}]({model_req['source']}).")
 st.markdown("Built with love by [NLRC 510](https://www.510.global/). See [the project on GitHub](https://github.com/rodekruis/text-generation-app).")
 nclass = st.number_input('How many classes do you want to define?', min_value=0, max_value=10, value=0, step=1)
 if nclass > 0:
@@ -38,7 +39,6 @@ if nclass > 0:
             'text': build_prompt(class_names, class_examples, input_text),
             'length': max([len(x) for x in class_names])
         }
-        print(payload)
 
         with st.spinner(text="This may take a moment..."):
             result = requests.post(url, json=payload).json()
